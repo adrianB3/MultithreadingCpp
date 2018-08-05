386a9efdc3dfdc373f04a069b0010f16c786fb7f
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-08-05 12:40:53 +0300

    thread mng update

--- ThreadManagement/cmake-build-debug/CMakeFiles/ThreadManagement.dir/DependInfo.cmake
+++ ThreadManagement/cmake-build-debug/CMakeFiles/ThreadManagement.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/adrianb3/MEGA/Learning/AdvancedCpp/ThreadTutorial/ThreadManagement/main.cpp" "/home/adrianb3/MEGA/Learning/AdvancedCpp/ThreadTutorial/ThreadManagement/cmake-build-debug/CMakeFiles/ThreadManagement.dir/main.cpp.o"
+  "/home/adrianb3/Dev/Cpp/MultithreadingCpp/ThreadManagement/main.cpp" "/home/adrianb3/Dev/Cpp/MultithreadingCpp/ThreadManagement/cmake-build-debug/CMakeFiles/ThreadManagement.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- ThreadManagement/main.cpp
+++ ThreadManagement/main.cpp
@@ -12,34 +12,35 @@ public:
 
 int main() {
     string s = "Where there is no trust, there is no love.";
-    //thread t1((Fctor()), ref(s)); // t1 starts running , msg passed by reference
-    // RAII --> wrapper w(t1)
+    thread t1((Fctor()), ref(s)); // t1 starts running , msg passed by reference
+    // RAII (resource acquisition is initialization) --> wrapper w(t1)
+    // when t1 goes out of scope the destructor of the class joins t1 to the main thread
 
-    /*try {
+    try {
 
             cout << "from main: " << s << endl;
 
     } catch (...) {
         t1.join();
         throw ;
-    }*/
+    }
 
    // t1.join();
     //cout << "from main: " << s << endl;
 
-    // another way
+    // another way -- better
     /*thread t1((Fctor()), move(s));
     thread t2 = move(t1);
     t2.join();*/
 
 
-    /*cout << this_thread::get_id() << endl;
+    cout << this_thread::get_id() << endl;
     cout << t1.get_id();
-    t1.join();*/
+    t1.join();
 
-    // Avoid oversubscribing (creating more threads than that are hardware supported --> because of contact switching
-    cout << thread::hardware_concurrency() << endl; // no of threads that can truly run concurrently
-
+    // Avoid oversubscribing
+    // (creating more threads than that are hardware supported --> because of context switching overhead)
+    cout << thread::hardware_concurrency() << endl; // number of threads that can truly run concurrently
 
     return 0;
 }
